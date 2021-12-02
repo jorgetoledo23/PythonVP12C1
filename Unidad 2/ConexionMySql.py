79f8e25210bb75d7dd0763aac945fe5f05b71b6f
Jorge <jorge.toledo.6@hotmail.com>
2021-12-02 19:06:19 -0300

    Clase 2 Diciembre

--- Unidad 2/ConexionMySql.py
+++ Unidad 2/ConexionMySql.py
@@ -6,7 +6,7 @@ class DAO:
         try:
             self.cnx = mysql.connector.connect(user='root', password='',
                 host='127.0.0.1',
-                database='d_n2_p12_c1')
+                database='d_iei_p1_c1')
             #
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -16,6 +16,7 @@ class DAO:
             else:
                 print(err)
 
+    #CRUD
 
     def InsertarCliente(self, C):
         add_cliente = ("insert into tbl_clientes"
@@ -29,6 +30,35 @@ class DAO:
         cursor.execute(add_cliente, data_cliente)
         self.cnx.commit()
 
+    def ListarClientes(self):
+        cursor = self.cnx.cursor()
+        cursor.execute("SELECT * FROM tbl_clientes")
+        lista = []
+        for(rut, nombres, apellidos, correo, telefono, direccion, comuna) in cursor:
+            C = Cliente(rut, nombres, apellidos, correo, telefono, direccion, comuna)
+            lista.append(C)
+        return lista
+
+    def ActualizarCliente(self, C, Rut):
+        add_cliente = ("UPDATE tbl_clientes SET rut = %s, nombres = %s, apellidos = %s, correo = %s, "
+                    "telefono = %s, direccion = %s, comuna = %s "
+                    "WHERE rut = %s")
+        data_cliente = (C.getRut(),C.getNombres(),C.getApellidos(),C.getCorreo(),C.getTelefono(),C.getDireccion(),C.getComuna(), Rut)   
+        
+        cursor = self.cnx.cursor()
+        cursor.execute(add_cliente, data_cliente)
+        self.cnx.commit()
+
+    def EliminarCliente(self, Rut):
+
+        delete_cliente = ("DELETE FROM tbl_clientes WHERE Rut = %s")
+        data_cliente = (Rut,)
+
+        cursor = self.cnx.cursor()
+        cursor.execute(delete_cliente, data_cliente)
+        self.cnx.commit()
+
+
     def InsertarMecanico(self, M):
         add_mecanico = ("insert into tbl_mecanicos"
                         "(rut, nombres, apellidos, direccion, correo, telefono, comuna)"
@@ -41,15 +71,6 @@ class DAO:
         cursor.execute(add_mecanico, data_mecanico)
         self.cnx.commit()
 
-    def ListarClientes(self):
-        cursor = self.cnx.cursor()
-        cursor.execute("SELECT * FROM tbl_clientes")
-        lista = []
-        for(rut, nombres, apellidos, correo, telefono, direccion, comuna) in cursor:
-            C = Cliente(rut, nombres, apellidos, correo, telefono, direccion, comuna)
-            lista.append(C)
-        return lista
-
     def ListarMecanicos(self):
         cursor = self.cnx.cursor()
         cursor.execute("SELECT * FROM tbl_mecanicos")
@@ -58,10 +79,55 @@ class DAO:
             M = Mecanico(rut, nombres, apellidos, correo, telefono, direccion, comuna)
             lista.append(M)
         return lista
+
+    def ActualizarMecanico(self, C, Rut):
+        add_mecanico = ("UPDATE tbl_mecanicos SET rut = %s, nombres = %s, apellidos = %s, correo = %s, "
+                    "telefono = %s, direccion = %s, comuna = %s "
+                    "WHERE rut = %s")
+        data_mecanico = (C.getRut(),C.getNombres(),C.getApellidos(),C.getCorreo(),C.getTelefono(),C.getDireccion(),C.getComuna(), Rut)   
         
+        cursor = self.cnx.cursor()
+        cursor.execute(add_mecanico, data_mecanico)
+        self.cnx.commit()
+
+    def EliminarMecanico(self, Rut):
+
+        delete_mecanico = ("DELETE FROM tbl_mecanicos WHERE Rut = %s")
+        data_mecanico = (Rut,)
+
+        cursor = self.cnx.cursor()
+        cursor.execute(delete_mecanico, data_mecanico)
+        self.cnx.commit()
 
 
-    
+    def InsertarVehiculo(self, V):
+        add_vehiculo = ("insert into tbl_autos"
+                        "(patente, marca, modelo, year, color, numero_chasis, rut_cliente)"
+                        "values"
+                        "(%s,%s,%s,%s,%s,%s,%s)")
 
-#CREATE
-#RUD
+        data_vehiculo = (V.getPatente(), V.getMarca(), V.getModelo(), V.getYear(), V.getColor(), V.getChasis(), V.getCliente())
+
+        cursor = self.cnx.cursor()
+        cursor.execute(add_vehiculo, data_vehiculo)
+        self.cnx.commit()
+
+    def ListarVehiculos(self):
+        cursor = self.cnx.cursor()
+        cursor.execute("SELECT * FROM tbl_autos")
+        lista = []
+        for(patente, marca, modelo, year, color, numero_chasis, rut_cliente) in cursor:
+            A = Auto(patente, numero_chasis,color, marca, year, modelo, rut_cliente)
+            lista.append(A)
+        return lista
+
+    def ActualizarVehiculo(self, V, Patente):
+        pass
+
+    def EliminarVehiculo(self, Patente):
+        delete_auto = ("DELETE FROM tbl_autos WHERE Patente = %s")
+        data_auto = (Patente,)
+
+        cursor = self.cnx.cursor()
+        cursor.execute(delete_auto, data_auto)
+        self.cnx.commit()
--- Unidad 2/Sistema.py
+++ Unidad 2/Sistema.py
@@ -49,8 +49,65 @@ while True:
             print("")
             input("Presione Enter para Continuar...")
 
+        if oP == "3":
+            #Actualizar Cliente
+            mP.LimpiarConsola()
+            print("======= Editando Cliente =======")
+            print("=================================")
+            print("======= Seleccione el Cliente que desea Actualizar =======")
+            print("")
+
+            for C in dao.ListarClientes():
+                print(C.getInfoDetallada())
+
+            RutAntiguo = input("Digite Rut del Cliente: ")
+
+            mP.LimpiarConsola()
+            print(f"======= Editando Cliente: {RutAntiguo} =======")
+            print("============== Complete la Informacion ===================")
+            print("")
+
+            Rut = str(input("Rut: "))
+            Nombres = str(input("Nombres: "))
+            Apellidos = str(input("Apellidos: "))
+            Telefono = str(input("Telefono: "))
+            Correo = str(input("Correo: "))
+            Direccion = str(input("Direccion: "))
+            Comuna = str(input("Comuna: "))
+            C = Cliente(Rut,Nombres,Apellidos,Correo,Telefono,Direccion,Comuna)
+
+            dao.ActualizarCliente(C, RutAntiguo)
+            mP.ConfirmacionEdit('Cliente')
+
+        if oP == "4":
+            #Eliminar Cliente
+            mP.LimpiarConsola()
+            print("======= Eliminando Cliente =======")
+            print("=================================")
+            print("======= Seleccione el Cliente que desea Eliminar =======")
+            print("Cuidado, la data eliminada no se puede Recuperar!")
+            print("")
+
+            for C in dao.ListarClientes():
+                print(C.getInfoDetallada())
+
+            Rut = input("Digite Rut del Cliente: ")
+
+            confirmacion = input(f"Estas seguro de eliminar al Cliente Rut: {Rut} (Y/N): ")
+
+            if confirmacion == "Y":
+                dao.EliminarCliente(Rut)
+                mP.ConfirmacionDelete("Cliente")
+            
+            if confirmacion == "N":
+                print("Operacion Cancelada")
+                input("Presione Enter para Continuar...")
+
+
         if oP == "0":
             pass
+
+        oP == None
 
     if oP == "2":
         #Gestionamos Mecanicos
@@ -92,6 +149,138 @@ while True:
             print("")
             input("Presione Enter para Continuar...")
 
+        if oP == "3":
+            #Actualizar Mecanico
+            mP.LimpiarConsola()
+            print("======= Editando Mecanico =======")
+            print("=================================")
+            print("======= Seleccione el Mecanico que desea Actualizar =======")
+            print("")
+
+            for C in dao.ListarMecanicos():
+                print(C.getInfoDetallada())
+
+            RutAntiguo = input("Digite Rut del Mecanico: ")
+
+            mP.LimpiarConsola()
+            print(f"======= Editando Mecanico: {RutAntiguo} =======")
+            print("============== Complete la Informacion ===================")
+            print("")
+
+            Rut = str(input("Rut: "))
+            Nombres = str(input("Nombres: "))
+            Apellidos = str(input("Apellidos: "))
+            Telefono = str(input("Telefono: "))
+            Correo = str(input("Correo: "))
+            Direccion = str(input("Direccion: "))
+            Comuna = str(input("Comuna: "))
+            M = Mecanico(Rut,Nombres,Apellidos,Correo,Telefono,Direccion,Comuna)
+
+            dao.ActualizarMecanico(M, RutAntiguo)
+            mP.ConfirmacionEdit('Mecanico')
+
+
+        if oP == "4":
+            #Eliminar Mecanico
+            mP.LimpiarConsola()
+            print("======= Eliminando Mecanico =======")
+            print("=================================")
+            print("======= Seleccione el Mecanico que desea Eliminar =======")
+            print("Cuidado, la data eliminada no se puede Recuperar!")
+            print("")
+
+            for C in dao.ListarMecanicos():
+                print(C.getInfoDetallada())
+
+            Rut = input("Digite Rut del Mecanico: ")
+
+            confirmacion = input(f"Estas seguro de eliminar al Mecanico Rut: {Rut} (Y/N): ")
+
+            if confirmacion == "Y":
+                dao.EliminarMecanico(Rut)
+                mP.ConfirmacionDelete("Mecanico")
+            
+            if confirmacion == "N":
+                print("Operacion Cancelada")
+                input("Presione Enter para Continuar...")
+
 
         if oP == "0":
             pass
+
+        oP = None
+
+    if oP == "3":
+        #Gestionamos Vehiculos
+        mP.LimpiarConsola()
+        mP.MenuSecundario("Vehiculo")
+        oP = str(input(" : "))
+
+        if oP == "1":
+            #Ingresar nuevo Vehiculo
+            mP.LimpiarConsola()
+            print("======= Agregando Vehiculo =======")
+            print("Complete la Informacion Solicitada:")
+            print("")
+
+            Patente = input("Ingrese Patente: ")
+            Marca = input("Ingrese Marca: ")
+            Modelo = input("Ingrese Modelo: ")
+            Year = input("Ingrese Year: ")
+            NChasis =  input("Ingrese Numero de Chasis: ")
+            Color = input("Ingrese Color: ")
+
+            mP.LimpiarConsola()
+            print("======= Seleccion Cliente =======")
+            print("")
+
+            for C in dao.ListarClientes():
+                print(C.getInfo())
+
+            print("")
+        
+            rutCliente = input("Ahora Ingrese Rut del Cliente de la Lista Anterior: ")
+
+            A = Auto(Patente, NChasis, Color, Marca, Year, Modelo, rutCliente)
+
+            dao.InsertarVehiculo(A)
+
+            mP.ConfirmacionIngreso("Vehiculo")
+
+        if oP == "2":
+            #listar Vehiculos
+            mP.LimpiarConsola()
+            print("======= Listando Vehiculos =======")
+            print("=================================")
+            print("")
+
+            for A in dao.ListarVehiculos():
+                print(A.getInfo())
+
+            print("")
+            input("Presione Enter para Continuar...")
+
+
+        if oP == "4":
+            #Eliminar Vehiculo
+            mP.LimpiarConsola()
+            print("======= Eliminando Vehiculo =======")
+            print("=================================")
+            print("======= Seleccione el Vehiculo que desea Eliminar =======")
+            print("Cuidado, la data eliminada no se puede Recuperar!")
+            print("")
+
+            for C in dao.ListarVehiculos():
+                print(C.getInfo())
+
+            Patente = input("Digite Patente del Vehiculo: ")
+
+            confirmacion = input(f"Estas seguro de eliminar al Vehiculo Rut: {Rut} (Y/N): ")
+
+            if confirmacion == "Y":
+                dao.EliminarVehiculo(Patente)
+                mP.ConfirmacionDelete("Vehiculo")
+            
+            if confirmacion == "N":
+                print("Operacion Cancelada")
+                input("Presione Enter para Continuar...")
